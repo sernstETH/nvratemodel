84750dbe48aa4bf33dc1776906276b1c50aad763
Stefan Ernst <129668687+sernstETH@users.noreply.github.com>
2023-06-29 11:26:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='nvratemodel',
       version='1.0',
-      description='Rate models to simulate the photo-physics of the Nitrogen-Vacancy center in diamond.',
+      description='Numerical rate models to simulate the photo-physics of the Nitrogen-Vacancy (NV) center in diamond.',
       author='Stefan Ernst',
       author_email='sternst@ethz.ch',
       license='all rights reserved',
